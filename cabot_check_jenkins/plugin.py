9f74913236bb21a9950c51bd35c078e7efbf8bef
Jonathan Balls <j.balls@btopenworld.com>
2016-05-06 17:32:31 +0100

    Initial jenkins plugin commit

---
+++ cabot_check_jenkins/plugin.py
@@ -0,0 +1,106 @@
+from django.conf import settings
+from django.template import Context, Template
+from django import forms
+from cabot.plugins.models import CheckPlugin
+from cabot.plugins.forms import CheckConfigForm
+from cabot.cabotapp.models import StatusCheckResult
+from os import environ as env
+from datetime import datetime
+import logging
+import requests
+
+
+class JenkinsStatusCheckForm(CheckConfigForm):
+    max_queued_build_time = forms.IntegerField(
+	help_text = 'Alert if build queued for more than this many minutes.'
+    )
+
+
+class JenkinsCheckPlugin(CheckPlugin):
+    name = "Jenkins"
+    slug = "jenkins"
+    author = "Jonathan Balls"
+    version = "0.0.1"
+    font_icon = "glyphicon glyphicon-ok"
+
+    config_form = JenkinsStatusCheckForm
+
+    plugin_variables = [
+	'JENKINS_API',
+	'JENKINS_USER',
+	'JENKINS_PASS' 
+    ]
+
+    def run(self, check, result):
+
+        try:
+            status = self.get_job_status(self.name)
+            active = status['active']
+            result.job_number = status['job_number']
+            if status['status_code'] == 404:
+                result.error = u'Job %s not found on Jenkins' % self.name
+                result.succeeded = False
+                return result
+            elif status['status_code'] > 400:
+                # Will fall through to next block
+                raise Exception(u'returned %s' % status['status_code'])
+        except Exception as e:
+            # If something else goes wrong, we will *not* fail - otherwise
+            # a lot of services seem to fail all at once.
+            # Ugly to do it here but...
+            result.error = u'Error fetching from Jenkins - %s' % e.message
+            result.succeeded = True
+            return result
+
+        if not active:
+            # We will fail if the job has been disabled
+            result.error = u'Job "%s" disabled on Jenkins' % self.name
+            result.succeeded = False
+        else:
+            if self.max_queued_build_time and status['blocked_build_time']:
+                if status['blocked_build_time'] > self.max_queued_build_time * 60:
+                    result.succeeded = False
+                    result.error = u'Job "%s" has blocked build waiting for %ss (> %sm)' % (
+                        self.name,
+                        int(status['blocked_build_time']),
+                        self.max_queued_build_time,
+                    )
+                else:
+                    result.succeeded = status['succeeded']
+            else:
+                result.succeeded = status['succeeded']
+            if not status['succeeded']:
+                if result.error:
+                    result.error += u'; Job "%s" failing on Jenkins' % self.name
+                else:
+                    result.error = u'Job "%s" failing on Jenkins' % self.name
+                result.raw_data = status
+        return result
+
+    def get_job_status(self, check):
+	auth = (settings.JENKINS_USER, settings.JENKINS_PASS)
+
+	jobname = self.name
+	ret = {
+	    'active': True,
+	    'succeeded': False,
+	    'blocked_build_time': None,
+	    'status_code': 200
+	}
+	endpoint = settings.JENKINS_API + 'job/%s/api/json' % jobname
+	resp = requests.get(endpoint, auth=auth, verify=True)
+	status = resp.json()
+	ret['status_code'] = resp.status_code
+	ret['job_number'] = status['lastBuild'].get('number', None)
+	if status['color'].startswith('blue') or status['color'].startswith('green'): # Jenkins uses "blue" for successful; Hudson uses "green"
+	    ret['active'] = True
+	    ret['succeeded'] = True
+	elif status['color'] == 'disabled':
+	    ret['active'] = False
+	    ret['succeeded'] = False
+	if status['queueItem'] and status['queueItem']['blocked']:
+	    time_blocked_since = datetime.utcfromtimestamp(
+		float(status['queueItem']['inQueueSince']) / 1000).replace(tzinfo=timezone.utc)
+	    ret['blocked_build_time'] = (timezone.now() - time_blocked_since).total_seconds()
+	return ret
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='cabot-check-jenkins',
+      version='0.0.1',
+      description='A jenkins check plugin for Cabot by Arachnys',
+      author='Arachnys',
+      author_email='info@arachnys.org',
+      url='http://cabotapp.com',
+      packages=find_packages(),
+      download_url= 'https://github.com/bonniejools/cabot-check-icmp/tarball/0.0.1',
+     )
